eda0d25159bdfa9a00e01cc8ea4a407e2b706dcc
Yash Mahajan <yashmahajan99@gmail.com>
2023-10-13 14:44:12 +0530

    First Commit

---
+++ GitDemoProject/src/Testing/HelloWorld.java
@@ -0,0 +1,12 @@
+package Testing;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello All");
+		System.out.println("Software Testing...");
+		
+	}
+
+}
