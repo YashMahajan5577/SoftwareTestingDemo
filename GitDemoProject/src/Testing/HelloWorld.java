1db9e07d7c1b4d5751290c1f5f42531a0e72ed1a
YashMahajan5577 <107830089+YashMahajan5577@users.noreply.github.com>
2023-10-15 13:14:29 +0530

    Update HelloWorld.java

--- GitDemoProject/src/Testing/HelloWorld.java
+++ GitDemoProject/src/Testing/HelloWorld.java
@@ -3,7 +3,10 @@ package Testing;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		
+		for(int i=1;i<=5;i++)
+		{
+			System.out.println("Velocity");
+		}
 		System.out.println("Hello All");
 		System.out.println("Software Testing...");
 		System.out.println("issue found..");
