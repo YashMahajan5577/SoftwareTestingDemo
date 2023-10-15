f9c4b7b073763662bf348aac907862543bdf3c8a
YashMahajan5577 <107830089+YashMahajan5577@users.noreply.github.com>
2023-10-15 12:55:06 +0530

    Update HelloWorld.java

--- GitDemoProject/src/Testing/HelloWorld.java
+++ GitDemoProject/src/Testing/HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld {
 		System.out.println("Software Testing...");
 		System.out.println("issue found..");
 		System.out.println("updating the code...");
-		System.out.println("fixed");
+
 	}
 
 }
